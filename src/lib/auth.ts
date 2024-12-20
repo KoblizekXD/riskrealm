2df5f6dccfc72a379756368a020cccdedc426168
KoblizekXD <janprokupek@centrum.cz>
2024-12-20 15:18:01 +0100

    Register prisma adapter

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,8 +1,11 @@
+import { PrismaAdapter } from "@auth/prisma-adapter"
 import NextAuth from "next-auth"
 import GitHub from "next-auth/providers/github"
 import Google from "next-auth/providers/google"
+import prisma from "./db-util"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [
     GitHub({
       clientId: process.env.AUTH_GITHUB_CLIENT_ID,
