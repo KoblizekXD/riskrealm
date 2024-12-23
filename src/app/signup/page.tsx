025ed952c146ed0c4296a0a6e9be9db75e8913de
KoblizekXD <janprokupek@centrum.cz>
2024-12-23 09:24:06 +0100

    Convert signup page to client component

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,13 +1,20 @@
-import { providerMap, signIn } from "@/lib/auth";
-import { GitBranch } from "lucide-react";
-import { AuthError } from "next-auth";
-import Image from "next/image";
-import { redirect } from "next/navigation";
+'use client'
 
-export default async function SignUp(props: { searchParams: { callbackUrl: string | undefined, error: string | undefined } }) {
-  const params = await props.searchParams;
+import { GenericErrorPopup } from "@/components/generic-error";
+import { providerMap, translateError } from "@/lib/auth";
+import { AuthError } from "next-auth";
+import { signIn } from "next-auth/react";
+import Image from "next/image";
+import { redirect, useRouter } from "next/navigation";
+import { useState } from "react";
+
+export default function SignUp(props: { searchParams: { callbackUrl: string | undefined, error: string | undefined } }) {
+  const [error, setError] = useState<string | undefined>(undefined);
+  const router = useRouter();
+
   return (
     <main className={'min-h-screen md:h-screen lg:bg-black from-[#1e1e2e] to-[#181825] bg-gradient-to-br flex justify-center p-16 items-center'}>
+      {error && <GenericErrorPopup onClose={() => setError(undefined)} closeable message={translateError(error)} />}
       <div className={'w-full h-full z-10 flex flex-col gap-x-2 lg:flex-row p-2'}>
         <div className="flex-1 flex justify-center flex-col items-center">
           <h1 className={'text-4xl mt-12 font-extrabold text-center'}>Let's start your journey here!</h1>
@@ -20,7 +27,21 @@ export default async function SignUp(props: { searchParams: { callbackUrl: strin
             <span className={'from-yellow-300 text-transparent to-yellow-600 bg-gradient-to-r bg-clip-text font-bold'}> 500 tickets </span>
             by registering.
           </h3>
-          <form className={'mt-24 lg:w-[70%] flex flex-col gap-y-4'}>
+          <form action={(fd: FormData) => {
+            if (!fd.get('age-required')) {
+              setError('You must comply with the age requirement in order to register.');
+              return
+            }
+            signIn('credentials', {
+              email: fd.get('email') as string,
+              password: fd.get('password') as string,
+              redirect: false
+            }).then((res) => {
+              if (res?.error) {
+                setError(res.error);
+              }
+            });
+          }} className={'mt-24 lg:w-[70%] flex flex-col gap-y-4'}>
             <label className={'mt-4 font-semibold'}>
               <span>Email</span>
               <input
@@ -36,11 +57,11 @@ export default async function SignUp(props: { searchParams: { callbackUrl: strin
                 required
                 type={'password'}
                 name={'password'}
-                className={'block border-[#313244] border bg-[#11111b] rounded w-full p-2 mt-1 outline-none'}
+                className={'block border-[#5d5e6e] border bg-[#11111b] rounded w-full p-2 mt-1 outline-none'}
               />
             </label>
             <label className={'flex gap-x-4'}>
-              <input required type={'checkbox'} />
+              <input name="age-required" required type={'checkbox'} />
               <span>
                 I'm <span className="font-semibold">18 years</span> or older, have read our Terms Of Service,<br />
                 or <span className="font-semibold">was granted explicit permission to use</span>
@@ -53,7 +74,7 @@ export default async function SignUp(props: { searchParams: { callbackUrl: strin
             </button>
           </form>
         </div>
-        <div className={'flex lg:flex-col items-center gap-x-2 gap-y-2 py-4'}>
+        <div className={'flex select-none lg:flex-col items-center gap-x-2 gap-y-2 py-4'}>
           <div className="flex-1 border border-[#cdd6f4] rounded" />
           or
           <div className="flex-1 border border-[#cdd6f4] rounded" />
@@ -61,18 +82,15 @@ export default async function SignUp(props: { searchParams: { callbackUrl: strin
         <div className="flex-1 flex flex-col gap-y-8 justify-center items-center">
           {Object.values(providerMap).map((provider) => (
             <form className="text-center hover:bg-white hover:text-black transition-colors border flex rounded-md" key={provider.name}
-              action={async () => {
-                'use server'
-                try {
-                  await signIn(provider.id, {
-                    redirectTo: params?.callbackUrl ?? "",
-                  });
-                } catch (error) {
-                  if (error instanceof AuthError) {
-                    return redirect(`/signin?error=${error.type}`);
+              action={() => {
+                signIn(provider.id, {
+                  redirect: true,
+                  redirectTo: '/'
+                }).then(res => {
+                  if (res?.error) {
+                    setError(res.error);
                   }
-                  throw error // This thing has to be here for some reason???
-                }
+                });
               }}>
               <button
                 type={'submit'}
@@ -82,12 +100,11 @@ export default async function SignUp(props: { searchParams: { callbackUrl: strin
               </button>
             </form>
           ))}
-          <form action={'/signin'}>
-            <button
-              className={'bg-transparent border px-12 text-xl hover:text-black hover:bg-white transition-colors font-semibold text-white rounded flex items-center justify-center gap-x-2 p-2 mt-4'}>
-                I'm already registered!
-            </button>
-          </form>
+          <button
+            onClick={() => router.push('/signin')}
+            className={'bg-transparent border px-12 text-xl hover:text-black hover:bg-white transition-colors font-semibold text-white rounded flex items-center justify-center gap-x-2 p-2 mt-4'}>
+              I'm already registered!
+          </button>
         </div>
       </div>
       <div className={'fixed invisible lg:visible w-full brightness-[30%] -translate-y-16 h-full -rotate-2'}>
