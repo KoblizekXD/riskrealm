a772f58205cf1b36398428b3c30fa19f8d1f0d53
MalkiNcz <jakubmalek07@gmail.com>
2025-01-26 17:06:25 +0100

    Update laning page color scheme

--- src/app/home/landingpage.tsx
+++ src/app/home/landingpage.tsx
@@ -2,8 +2,8 @@
 
 export default function LandingPage() {
     return (
-        <div className="h-screen bg-black text-white flex flex-col">
-            <header className="h-16 bg-gradient-to-r from-lime-600 to-cyan-600 shadow-lg flex items-center justify-between px-6">
+        <div className="h-screen lg:bg-black from-[#1e1e2e] to-[#181825] bg-linear-to-br text-[#cdd6f4] flex flex-col">
+            <header className="h-16 bg-[#151520] shadow-lg flex items-center justify-between px-6">
                 <div className="flex items-center space-x-4">
                     <div className="text-2xl font-bold text-white">Risk Realm</div>
                 </div>
@@ -13,27 +13,27 @@ export default function LandingPage() {
                     <button className="text-white text-lg hover:text-lime-400 hover:scale-115 transition transform cursor-pointer">About Us</button>
                 </nav>
                 <div>
-                    <button className="bg-lime-500 text-black font-semibold py-1 px-4 rounded hover:bg-lime-400 cursor-pointer"
+                    <button className="bg-lime-500 text-black font-semibold py-1 px-4 rounded hover:bg-lime-400 cursor-pointer hover:scale-110"
                         onClick={() => {}}>                    
                         Sign In
                     </button>
                 </div>
             </header>
 
-            <main className="relative flex-grow p-8 flex flex-col items-center justify-center">
-            <h1 className="text-5xl font-extrabold bg-gradient-to-r from-lime-400 to-cyan-400 text-transparent bg-clip-text mb-2">
+            <main className="relative flex-grow p-8 flex flex-col items-center">
+            <h1 className="text-5xl font-extrabold bg-gradient-to-r from-lime-400 to-cyan-400 text-transparent bg-clip-text mb-2 pt-14">
                     Welcome to Risk Realm
                 </h1>
                 <p className="text-gray-300 text-lg text-center mb-8 max-w-3xl">
                     Lets go gamble!!!
                 </p>
                 <div className="flex space-x-4">
-                    <button className="bg-lime-500 text-black font-semibold py-2 px-6 rounded-xl shadow-lg hover:bg-lime-400 cursor-pointer"
+                    <button className="bg-lime-500 text-black font-semibold py-2 px-6 rounded-xl shadow-lg hover:bg-lime-400 cursor-pointer hover:scale-110"
                     onClick={() => {}}>
                   
                         Play Now
                     </button>
-                    <button className="bg-gray-800 border border-lime-500 text-lime-500 font-semibold py-2 px-6 rounded-xl shadow-lg hover:bg-gray-700 cursor-pointer"
+                    <button className="bg-gray-800 border border-lime-500 text-lime-500 font-semibold py-2 px-6 rounded-xl shadow-lg hover:bg-gray-700 cursor-pointer hover:scale-110"
                     onClick={() => {}}>
                         Learn More
                     </button>
@@ -55,7 +55,7 @@ export default function LandingPage() {
                 </div>
             </main>
 
-            <footer className="h-20 flex items-center justify-center border-t border-gray-800">
+            <footer className="h-20 flex items-center justify-center border-t border-gray-800 bg-[#181825]">
                 <p className="text-gray-400 text-sm">© 2025 Risk Realm. All Rights Reserved. Gamble until zero.</p>
             </footer>
         </div>
