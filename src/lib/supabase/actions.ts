d259f6fc400108ca657d3886b7860dc10311de03
KoblizekXD <janprokupek@centrum.cz>
2025-01-26 21:05:06 +0100

    Add signing capabilities

--- src/lib/supabase/actions.ts
+++ src/lib/supabase/actions.ts
@@ -2,27 +2,49 @@
 
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
+import { type ZodError, z } from "zod";
 import { createClient } from "./server";
 
-export async function login(formData: FormData) {
+export const signUpSchema = z.object({
+  email: z.string().email(),
+  full_name: z.string().min(2),
+  password: z.string().min(8),
+});
+
+export const loginSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(8),
+});
+
+export async function login(
+  formData: FormData,
+  navigateTo?: string
+): Promise<undefined | string | ZodError<z.infer<typeof loginSchema>>> {
   const supabase = await createClient();
 
-  const data = {
+  const data = loginSchema.safeParse({
     email: formData.get("email") as string,
     password: formData.get("password") as string,
-  };
+  });
 
-  const { error } = await supabase.auth.signInWithPassword(data);
+  if (!data.success) {
+    return data.error;
+  }
+
+  const { error } = await supabase.auth.signInWithPassword(data.data);
 
   if (error) {
-    redirect("/error");
+    return error.message;
   }
 
   revalidatePath("/", "layout");
-  redirect("/account");
+  if (navigateTo) redirect(navigateTo);
 }
 
-export async function signup(formData: FormData) {
+export async function signup(
+  formData: FormData,
+  navigateTo?: string
+): Promise<undefined | string | ZodError<z.infer<typeof signUpSchema>>> {
   const supabase = await createClient();
 
   const data = {
@@ -33,9 +55,9 @@ export async function signup(formData: FormData) {
   const { error } = await supabase.auth.signUp(data);
 
   if (error) {
-    redirect("/error");
+    return error.message;
   }
 
   revalidatePath("/", "layout");
-  redirect("/account");
+  if (navigateTo) redirect(navigateTo);
 }
