e3496738af8c4c8529826c30b91b413f7aea12c2
MalkiNcz <jakubmalek07@gmail.com>
2025-03-01 13:45:28 +0100

    Update balance management and simplify betting logic in Plinko game

--- src/app/plinko/game/game.tsx
+++ src/app/plinko/game/game.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useEffect, useState, ChangeEvent } from 'react';
 import {
   Bodies,
   Body,
@@ -12,14 +12,15 @@ import {
   Runner,
   World,
 } from 'matter-js';
-import { useAuthStore } from './store';
+
 import { useGameStore } from './store';
-import { BetActions, PlinkoGameBody, MultiplierHistory } from './components';
+import { PlinkoGameBody, MultiplierHistory } from './components';
 import { config, getMultiplierByLinesQnt } from './config';
-import { updateBalance, calculateMultiplier, canAddBall } from '../../../lib/games/plinko';
-type MultiplierValues = 110 | 88 | 41 | 33 | 25 | 18 | 15 | 10 | 5 | 3 | 2 | 1.5 | 1 | 0.5 | 0.3;
+import {incrementBalance, decrementBalance, calcWin, canAddBall } from '../../../lib/games/plinko';
+import { canClaimStreak, updateBalance } from "@/lib/supabase/actions";
 
 type LinesType = 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15 | 16;
+type MultiplierValues = 110 | 88 | 41 | 33 | 25 | 18 | 15 | 10 | 5 | 3 | 2 | 1.5 | 1 | 0.5 | 0.3;
 
 export function Plinko() {
   // #region States
@@ -28,6 +29,166 @@ export function Plinko() {
   const incrementInGameBallsCount = useGameStore((state) => state.incrementGamesRunning);
   const decrementInGameBallsCount = useGameStore((state) => state.decrementGamesRunning);
   const [lastMultipliers, setLastMultipliers] = useState<number[]>([]);
+  const [playerBalance, setPlayerBalance] = useState<number>(100000);
+ 
+    
+
+  interface PlinkoBetActions {
+    onRunBet: (betValue: number) => void
+    onChangeLines: (lines: LinesType) => void
+    inGameBallsCount: number
+  }
+  
+  function BetActions({
+    onRunBet,
+    onChangeLines,
+    inGameBallsCount
+  }: PlinkoBetActions) {
+  
+    const [betValue, setBetValue] = useState(0)
+    const maxLinesQnt = 16
+    const linesOptions: number[] = []
+    
+    
+    for (let i = 8; i <= maxLinesQnt; i++) {
+      linesOptions.push(i)
+    }
+  
+    function handleChangeBetValue(e: ChangeEvent<HTMLInputElement>) {
+  
+      e.preventDefault()
+      const value = +e.target.value
+      const newBetValue = value >= playerBalance ? playerBalance : value
+      setBetValue(newBetValue)
+    }
+  
+    function handleChangeLines(e: ChangeEvent<HTMLSelectElement>) {
+      
+  
+      onChangeLines(Number(e.target.value) as LinesType)
+    }
+  
+    function handleHalfBet() {
+   
+      const value = betValue / 2
+      const newBetvalue = value <= 0 ? 0 : Math.floor(value)
+      setBetValue(newBetvalue)
+    }
+  
+    function handleDoubleBet() {
+      
+      const value = betValue * 2
+  
+      if (value >= playerBalance) {
+        setBetValue(playerBalance)
+        return
+      }
+  
+      const newBetvalue = value <= 0 ? 0 : Math.floor(value)
+      setBetValue(newBetvalue)
+    }
+  
+    function handleMaxBet() {
+  
+      setBetValue(playerBalance)
+    }
+
+    async function handleRunBet() {
+      
+      if (inGameBallsCount >= 15) return
+      if (betValue > playerBalance) {
+        setBetValue(playerBalance)
+        return
+      }
+      onRunBet(betValue)
+      if (betValue <= 0) return
+      //setPlayerBalance(await decrementBalance(betValue, playerBalance));
+    }
+  
+    
+  
+    return (
+      <div className="relative h-1/2 w-full flex-1 py-8 px-4 bg-cyan-500">
+        <span className="absolute left-4 top-0 mx-auto text-xs font-bold text-black md:text-base">
+          balls: {inGameBallsCount.toFixed(0)}/15
+        </span>
+        <br></br>
+        <span className="left-4 top-0 mx-auto text-xs font-bold text-black md:text-base">
+         Balance: {playerBalance.toFixed(0)}
+        </span>
+  
+        <div className="flex h-full flex-col gap-4 rounded-md bg-primary p-4 text-text md:justify-between">
+          <div className="flex flex-col gap-2">
+            <div className="flex flex-row items-stretch gap-1 md:flex-col">
+              <div className="w-full text-sm font-bold md:text-base">
+                <div className="flex flex-1 items-stretch justify-between">
+                  <span>Place a Bet</span>
+                  <div className="flex items-center gap-1">
+                    <div className="rounded-full bg-purpleDark p-0.5">
+                    </div>
+                    <span>{betValue.toFixed(0)}</span>
+                  </div>
+                </div>
+                <div className="flex items-stretch justify-center shadow-md">
+                  <input
+                    type="number"
+                    min={0}
+                    max={playerBalance}
+                    onChange={handleChangeBetValue}
+                    value={betValue}
+                    className="w-full rounded-bl-md rounded-tl-md border-2 border-secondary bg-background p-2.5 px-4 font-bold transition-colors placeholder:font-bold placeholder:text-text focus:border-purple focus:outline-none md:p-2"
+                  />
+                  <button
+                    onClick={handleHalfBet}
+                    className="relative border-2 border-transparent bg-secondary p-2.5 px-3 transition-colors after:absolute after:top-[calc(50%_-_8px)] after:right-0 after:h-4 after:w-0.5 after:rounded-lg after:bg-background after:content-[''] hover:bg-secondary/80 focus:border-purple focus:outline-none md:p-2"
+                  >
+                    ½
+                  </button>
+                  <button
+                    onClick={handleDoubleBet}
+                    className="relative border-2 border-transparent bg-secondary p-2.5 px-3 transition-colors after:absolute after:top-[calc(50%_-_8px)] after:right-0 after:h-4 after:w-0.5 after:rounded-lg after:bg-background after:content-[''] hover:bg-secondary/80 focus:border-purple focus:outline-none md:p-2"
+                  >
+                    2x
+                  </button>
+                  <button
+                    onClick={handleMaxBet}
+                    className="rounded-br-md rounded-tr-md border-2 border-transparent bg-secondary p-2 px-3 text-xs transition-colors hover:bg-secondary/80 focus:border-purple focus:outline-none"
+                  >
+                    max
+                  </button>
+                </div>
+              </div>
+  
+              
+            </div>
+            <select
+              disabled={inGameBallsCount > 0}
+              onChange={handleChangeLines}
+              defaultValue={16}
+              className="w-full rounded-md border-2 border-secondary bg-background py-2 px-4 font-bold transition-all placeholder:font-bold placeholder:text-text focus:border-purple focus:outline-none disabled:line-through disabled:opacity-80"
+              id="lines"
+            >
+              {linesOptions.map(line => (
+                <option key={line} value={line}>
+                  {line} Lines
+                </option>
+              ))}
+            </select>
+          </div>
+          <button
+            onClick={handleRunBet}
+  
+            className="hidden rounded-md bg-purple px-6 py-5 font-bold leading-none text-background transition-colors hover:bg-purpleDark focus:outline-none focus:ring-1 focus:ring-purple focus:ring-offset-1 focus:ring-offset-primary disabled:bg-gray-500 md:visible md:block"
+          >
+           Bet
+          </button>
+          
+        </div>
+      </div>
+    )
+  }
+
+  
 
   const {
     pins: pinsConfig,
@@ -118,7 +279,8 @@ export function Plinko() {
         console.warn('Maximum number of balls reached (15)');
         return;
       }
-
+      setPlayerBalance(playerBalance - ballValue);
+      //console.log(`old balance: ${playerBalance}`);
       incrementInGameBallsCount();
 
       const minBallX = worldWidth / 2 - pinsConfig.pinSize * 3 + pinsConfig.pinGap;
@@ -239,8 +401,12 @@ export function Plinko() {
 
       if (+ballValue <= 0) return;
 
-      const newBalance = await calculateMultiplier(lines, +ballValue);
-      await updateBalance(newBalance);
+      const newWin = await calcWin(multiplierValue, +ballValue);
+      console.log(`New win: ${newWin}`);
+      console.log(`old balance: ${playerBalance}`);
+      setPlayerBalance(playerBalance + newWin);
+      await console.log(`New balance: ${playerBalance}`);
+      
     },
     [lines]
   );
--- src/lib/games/plinko.ts
+++ src/lib/games/plinko.ts
@@ -1,26 +1,23 @@
 "use server";
 
 
-import { getMultiplierByLinesQnt } from '../../app/plinko/game/config';
-
-type LinesType = 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15 | 16;
-type MultiplierValues = 110 | 88 | 41 | 33 | 25 | 18 | 15 | 10 | 5 | 3 | 2 | 1.5 | 1 | 0.5 | 0.3;
-
 // Simulate a server-side balance store
-let serverBalance = 9999;
+//let serverBalance = 9999;
 
 // Function to update the balance securely
-export async function updateBalance(amount: number): Promise<number> {
-  serverBalance += amount;
-  return serverBalance;
+export async function incrementBalance(amount: number, balance: number) {
+  balance += amount;
+  return balance;
 }
 
+export async function decrementBalance(amount: number, balance: number) {
+    balance -= amount;
+    return balance;
+  }
+
 // Function to calculate the multiplier securely
-export async function calculateMultiplier(lines: LinesType, ballValue: number): Promise<number> {
-  const multipliers = getMultiplierByLinesQnt(lines);
-  const randomIndex = Math.floor(Math.random() * multipliers.length);
-  const multiplierValue = +multipliers[randomIndex].label.split('-')[1] as MultiplierValues;
-  return ballValue * multiplierValue;
+export async function calcWin(multiplierValue: number, ballValue: number){
+  return +multiplierValue * ballValue;
 }
 
 // Function to validate if a new ball can be added
