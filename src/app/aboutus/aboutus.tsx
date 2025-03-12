7beedc84679991490dcd4a237319ba13d705106c
MalkiNcz <jakubmalek07@gmail.com>
2025-03-12 18:09:18 +0100

    Add content Component to simplify code

--- src/app/aboutus/aboutus.tsx
+++ src/app/aboutus/aboutus.tsx
@@ -23,41 +23,9 @@ import Link from "next/link";
 import { useEffect, useState } from "react";
 import Navbar from "@/components/navbar";
 
-
-
-export function AboutUsNotLogged() {
-    const router = useRouter();
-    return (
-        <div className="min-h-screen bg-gradient-to-b from-[#1a1124] to-[#110b18] text-[#D4AF37] flex flex-col overflow-hidden">
-          <div className="flex flex-col items-center">
-          <header className="md:h-20 bg-[#151520] shadow-lg flex flex-col items-center w-full justify-between px-2 md:px-6 md:flex-row">
-          <div className="flex items-center space-x-2 md:space-x-4 mt-4 md:mt-0 border-b-3 border-[#ce9aff] pb-2 md:pb-0 md:border-b-0">
-            <div className="text-4xl md:text-2xl font-bold text-[#d4af37]">
-              Risk Realm
-            </div>
-          </div>
-            <button
-              type="button"
-              onClick={() => router.push("/signin")}
-              className="bg-[#D4AF37] text-white font-bold px-3 py-2 rounded-lg hover:bg-[#d4bf37] hover:shadow-[0px_0px_15px_#FFD700] shadow-lg md:px-4 cursor-pointer hover:scale-110 transition transform text-sm mb-4 md:mb-0">
-              Sign In
-            </button>
-        </header>
-        <main
-  className={`relative text-center flex-grow p-4 lg:p-8 flex flex-col items-center overflow-y-auto mr-auto ml-auto max-w-[1550px] transition-all duration-300`}>
-  
-  <h1
-    className={`${luckiestGuy.className} text-4xl md:text-8xl font-extrabold text-[#D4AF37] drop-shadow-[0_0_5px_#CFAF4A] mb-4 pt-6 md:pt-10`}>
-    Meet our team!
-  </h1>
-
-  <Link
-    href="/"
-    className="mb-6 bg-[#D4AF37] text-black font-bold py-3 px-6 rounded-xl shadow-md hover:shadow-lg transition-all duration-300 hover:bg-[#FFD700]">
-    Back to Homepage
-  </Link>
-
-  <div className="flex flex-col items-center px-6 py-10 md:px-12">
+export function Content() {
+  return (
+    <><div className="flex flex-col items-center px-6 py-10 md:px-12">
     <div className="flex flex-col md:flex-row items-center md:space-x-8 mb-12">
       <div className="w-full md:w-1/2">
         <Image
@@ -127,7 +95,45 @@ export function AboutUsNotLogged() {
         </p>
       </div>
     </div>
-  </div>
+  </div></>
+  );
+}
+
+
+
+export function AboutUsNotLogged() {
+    const router = useRouter();
+    return (
+        <div className="min-h-screen bg-gradient-to-b from-[#1a1124] to-[#110b18] text-[#D4AF37] flex flex-col overflow-hidden">
+          <div className="flex flex-col items-center">
+          <header className="md:h-20 bg-[#151520] shadow-lg flex flex-col items-center w-full justify-between px-2 md:px-6 md:flex-row">
+          <div className="flex items-center space-x-2 md:space-x-4 mt-4 md:mt-0 border-b-3 border-[#ce9aff] pb-2 md:pb-0 md:border-b-0">
+            <div className="text-4xl md:text-2xl font-bold text-[#d4af37]">
+              Risk Realm
+            </div>
+          </div>
+            <button
+              type="button"
+              onClick={() => router.push("/signin")}
+              className="bg-[#D4AF37] text-white font-bold px-3 py-2 rounded-lg hover:bg-[#d4bf37] hover:shadow-[0px_0px_15px_#FFD700] shadow-lg md:px-4 cursor-pointer hover:scale-110 transition transform text-sm mb-4 md:mb-0">
+              Sign In
+            </button>
+        </header>
+        <main
+  className={`relative text-center flex-grow p-4 lg:p-8 flex flex-col items-center overflow-y-auto mr-auto ml-auto max-w-[1550px] transition-all duration-300`}>
+  
+  <h1
+    className={`${luckiestGuy.className} text-4xl md:text-8xl font-extrabold text-[#D4AF37] drop-shadow-[0_0_5px_#CFAF4A] mb-4 pt-6 md:pt-10`}>
+    Meet our team!
+  </h1>
+
+  <Link
+    href="/"
+    className="mb-6 bg-[#D4AF37] text-black font-bold py-3 px-6 rounded-xl shadow-md hover:shadow-lg transition-all duration-300 hover:bg-[#FFD700]">
+    Back to Homepage
+  </Link>
+  <Content />
+  
 </main>
 
           </div>
@@ -269,82 +275,11 @@ export function AboutUsLogged({ user }: { user: UserType }) {
           className={`relative text-center flex-grow p-4 lg:p-8 flex flex-col items-center overflow-y-auto mr-auto ml-auto max-w-[1550px] transition-all duration-300 ${
             isNavOpen ? "ml-64" : "ml-0"
           }`}>
-          <h1
-          className={`${luckiestGuy.className} text-4xl md:text-8xl font-extrabold text-[#D4AF37] drop-shadow-[0_0_5px_#CFAF4A] mb-4 pt-6 md:pt-10`}>
-          Meet our team!
-        </h1>
-
-        <div className="flex flex-col items-center px-6 py-10 md:px-12">
-          <div className="flex flex-col md:flex-row items-center md:space-x-8 mb-12">
-            <div className="w-full md:w-1/2">
-              <Image
-                src="/path.jpg"
-                alt="Honza"
-                width={600}
-                height={400}
-                className="rounded-lg shadow-lg"
-              />
-            </div>
-
-            <div className="w-full md:w-1/2 text-center md:text-left mt-4 md:mt-0">
-              <h3 className="text-3xl font-bold text-[#D4AF37] drop-shadow-[0_0_5px_#CFAF4A] mb-4">
-                Jan Prokůpek
-              </h3>
-              <p className="text-[#D4AF37] text-sm md:text-base">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a
-                urna eget ligula laoreet volutpat sit amet nec justo. Integer
-                feugiat sapien nec feugiat pretium. Sed vestibulum cursus orci
-                sit amet suscipit.
-              </p>
-            </div>
-          </div>
-
-          <div className="flex flex-col md:flex-row-reverse items-center md:space-x-8 mb-12">
-            <div className="w-full md:w-1/2">
-              <Image
-                src="/path.jpg"
-                alt="Jakub"
-                width={600}
-                height={400}
-                className="rounded-lg shadow-lg"
-              />
-            </div>
-            <div className="w-full md:w-1/2 text-center md:text-left mt-4 md:mt-0">
-              <h3 className="text-3xl font-bold text-[#D4AF37] drop-shadow-[0_0_5px_#CFAF4A] mb-4">
-                Jakub Málek
-              </h3>
-              <p className="text-[#D4AF37] text-sm md:text-base">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a
-                urna eget ligula laoreet volutpat sit amet nec justo. Integer
-                feugiat sapien nec feugiat pretium. Sed vestibulum cursus orci
-                sit amet suscipit.
-              </p>
-            </div>
-          </div>
-
-          <div className="flex flex-col md:flex-row items-center md:space-x-8 mb-12">
-            <div className="w-full md:w-1/2">
-              <Image
-                src="/path.jpg"
-                alt="Jarin"
-                width={600}
-                height={400}
-                className="rounded-lg shadow-lg"
-              />
-            </div>
-            <div className="w-full md:w-1/2 text-center md:text-left mt-4 md:mt-0">
-              <h3 className="text-3xl font-bold text-[#D4AF37] drop-shadow-[0_0_5px_#CFAF4A] mb-4">
-                Jaroslav Rašovský
-              </h3>
-              <p className="text-[#D4AF37] text-sm md:text-base">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a
-                urna eget ligula laoreet volutpat sit amet nec justo. Integer
-                feugiat sapien nec feugiat pretium. Sed vestibulum cursus orci
-                sit amet suscipit.
-              </p>
-            </div>
-          </div>
-        </div>
+            <h1
+    className={`${luckiestGuy.className} text-4xl md:text-8xl font-extrabold text-[#D4AF37] drop-shadow-[0_0_5px_#CFAF4A] mb-4 pt-6 md:pt-10`}>
+    Meet our team!
+  </h1>
+          <Content />
         </main>
       </div>
 
