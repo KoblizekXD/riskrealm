227c0c2d26f294e98b0bcc29d71393e4e930503a
MalkiNcz <jakubmalek07@gmail.com>
2025-03-12 15:38:02 +0100

    Improve look of navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import Link from "next/link";
+import { JSX } from "react";
+import { FaHome, FaCalendarAlt, FaGamepad, FaUser, FaCog, FaTimes } from "react-icons/fa";
 
 interface NavbarProps {
   isOpen: boolean;
@@ -22,38 +24,32 @@ export default function Navbar({ isOpen, toggleNav }: NavbarProps) {
             type="button"
             onClick={toggleNav}
             className="text-4xl md:text-3xl font-bold text-[#d4af37] cursor-pointer hover:scale-110 transition-transform">
-            X
+            <FaTimes />
           </button>
         </div>
       </div>
 
-      <ul className="p-4">
-        <li className="mb-2">
-          <Link href="/" className="text-[#D4AF37] hover:text-[#FFD700]">
-            Home
-          </Link>
-        </li>
-        <li className="mb-2">
-          <Link href="/event" className="text-[#D4AF37] hover:text-[#FFD700]">
-            Event
-          </Link>
-        </li>
-        <li className="mb-2">
-          <Link href="/games" className="text-[#D4AF37] hover:text-[#FFD700]">
-            Games
-          </Link>
-        </li>
-        <li className="mb-2">
-          <Link href="/profile" className="text-[#D4AF37] hover:text-[#FFD700]">
-            Profile
-          </Link>
-        </li>
-        <li className="mb-2">
-          <Link href="/settings" className="text-[#D4AF37] hover:text-[#FFD700]">
-            Settings
-          </Link>
-        </li>
+      <ul className="w-full space-y-4 pl-4">
+        <NavItem href="/" icon={<FaHome />} text="Home" />
+        <NavItem href="/event" icon={<FaCalendarAlt />} text="Event" />
+        <NavItem href="/games" icon={<FaGamepad />} text="Games" />
+        <NavItem href="/profile" icon={<FaUser />} text="Profile" />
+        <NavItem href="/settings" icon={<FaCog />} text="Settings" />
       </ul>
     </div>
   );
 }
+
+function NavItem({ href, icon, text }: { href: string; icon: JSX.Element; text: string }) {
+  return (
+    <li>
+      <Link
+        href={href}
+        className="flex items-center gap-3 text-lg text-[#D4AF37] hover:text-[#FFD700] transition-colors p-2 w-full rounded-lg hover:bg-[#222]">
+        <span className="text-xl">{icon}</span>
+        <span>{text}</span>
+      </Link>
+    </li>
+  );
+}
+
